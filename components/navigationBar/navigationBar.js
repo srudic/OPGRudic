9552d13819d7239f5972b4ed67f4d845d3294fa0
Sanja Rudic <rudic.sanja@gmail.com>
2022-01-08 19:15:56 +0100

    Footer navigation designed

--- components/navigationBar/navigationBar.js
+++ components/navigationBar/navigationBar.js
@@ -1,35 +1,39 @@
 import Link from "next/link";
+import style from "../../styles/Navigation.module.css";
 
 const NavigationBar = () => {
   return (
-    <div>
-      <ul>
-        <li>
+    <div className={style.navigation}>
+      <div className={style.navigation_subcontainer}>
+        <li className={style.topPage}>
           <Link href={"/"}>Naslovnica</Link>
         </li>
-
-        <li>
+      </div>
+      <div className={style.navigation_subcontainer}>
+        <li className={style.topPage}>
           <Link href={"/products"}>Proizvodi</Link>
         </li>
-        <li>
+        <li className={style.subPage}>
           <Link href={"/products/oliveoil"}>Maslinovo ulje</Link>
         </li>
-        <li>
+        <li className={style.subPage}>
           <Link href={"/products/almonds"}>Bademi</Link>
         </li>
-        <li>
+      </div>
+      <div className={style.navigation_subcontainer}>
+        <li className={style.topPage}>
           <Link href={"/aboutUs"}>O nama</Link>
         </li>
-        <li>
+        <li className={style.subPage}>
           <Link href={"/aboutUs/fromFirstHand"}>Saznajte iz prve ruke</Link>
         </li>
-        <li>
+        <li className={style.subPage}>
           <Link href={"/aboutUs/landLocation"}>Lokacije nasada</Link>
         </li>
-        <li>
+        <li className={style.subPage}>
           <Link href={"/aboutUs/contact"}>Kontakt</Link>
         </li>
-      </ul>
+      </div>
     </div>
   );
 };
