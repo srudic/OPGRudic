fbfa86361b73168bbd1b80550d29e4ef10c24f9b
Sanja Rudic <rudic.sanja@gmail.com>
2022-02-14 18:05:08 +0100

    Blog Link made

--- components/blog/blogWrapper.js
+++ components/blog/blogWrapper.js
@@ -1,6 +1,32 @@
+import Link from "next/link";
 import styles from "../../styles/Blog/BlogWrapper.module.css";
+import { Blog as BLOG_CONSTANTS } from "../../constants/BlogConstants";
+
+const BlogItem = (props) => {
+  return (
+    <Link href={"/Saznajte_iz_prve_ruke/" + props.to}>
+      <div className={styles.ItemContainer}>
+        <h3>{props.subtitle}</h3>
+        <h2>{props.title}</h2>
+      </div>
+    </Link>
+  );
+};
+
 const BlogWrapper = () => {
-  return <div className={styles.WrapperContainer}>BlogWrapper</div>;
+  console.log(BLOG_CONSTANTS);
+  return (
+    <div className={styles.wrapperContainer}>
+      {BLOG_CONSTANTS.map((blogItem) => (
+        <BlogItem
+          key={blogItem.to}
+          to={blogItem.to}
+          title={blogItem.title}
+          subtitle={blogItem.subtitle}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BlogWrapper;
--- constants/BlogConstants.js
+++ constants/BlogConstants.js
@@ -1,7 +1,9 @@
 export const Blog = [
   {
-    to: "/Prica-1",
+    to: "prica-1",
     title: "Nasa prva prica",
+    subtitle:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     paragraph1:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     paragraph2:
